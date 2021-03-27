c8feda4c7c7bc36ea3ffd48584b1095128ecac12
ugurkryl41 <ugurkarayel41@gmail.com>
2021-03-27 03:09:44 +0300

    CarRentalProject

--- Business/ValidationRules/FluentValidation/BrandValidator.cs
+++ Business/ValidationRules/FluentValidation/BrandValidator.cs
@@ -15,12 +15,8 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(p => p.BrandName).NotEmpty();
             RuleFor(p => p.BrandName).MinimumLength(3);
             RuleFor(p => p.BrandName).NotNull();
-            RuleFor(p => p.BrandName).Must(test);
         }
 
-        private bool test(string arg)
-        {
-            return arg.StartsWith("A");
-        }
     }
 }
+
